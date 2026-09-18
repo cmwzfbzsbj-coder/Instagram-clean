aaea6752433252f019a590960430fd5db87c4a67
cmwzfbzsbj-coder <cmwzfbzsbj@privaterelay.appleid.com>
2026-09-18 11:18:55 +0100

    Create instagram-clean.js

---
+++ instagram-clean.js
@@ -0,0 +1,9 @@
+npm install
+npm test
+npm run check
+git init - b main
+git add .
+git commit - m "Add Instagram Clean userscript"
+git remote add origin https : //github.com/cmwzfbzsbj-coder/Instagram-clean.git
+git push - u origin main
+
